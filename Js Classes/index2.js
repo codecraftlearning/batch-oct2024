44fac6a9f57397864615cdcb3197547ed8c6d225
codecraftlearning <codecraftlearning@gmail.com>
2024-11-13 19:57:48 +0530

    hoisting class

---
+++ Js Classes/index2.js
@@ -0,0 +1,56 @@
+
+// console.log(test);
+// console.log(age);
+// console.log(height);
+
+// var test = "test code";
+// var age = 12;
+// var height = 5;
+
+
+/*
+
+Hoisting is the default behavior of JavaScript where all the variable and function declarations are moved on top. 
+In simple words, we can say that Hoisting is a process in which, irrespective of where the variables and functions are declared, 
+they are moved on top of the scope. The scope can be both local and global.
+
+*/
+
+
+batchName = "code craft learning";
+console.log(batchName);
+var batchName = undefined;
+console.log(batchName);
+
+
+
+
+/*
+
+var batchName = undefined;
+batchName = "code craft learning";
+console.log(batchName);
+batchName = undefined;
+console.log(batchName);
+
+*/
+
+
+
+
+/*
+
+var test = undefined;
+var age = undefined;
+var height = undefined;
+
+console.log(test);
+console.log(age);
+console.log(height);
+
+test = "test code";
+age = 12;
+height = 5;
+
+
+*/
---
+++ Js Classes/index3.js
@@ -0,0 +1,12 @@
+
+var test = "test code";
+
+
+function checkData() {
+    var innerData = undefined;
+    console.log(innerData);
+    innerData = "some data";
+}
+checkData();
+
+console.log(test);
